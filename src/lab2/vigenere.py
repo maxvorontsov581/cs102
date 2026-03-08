55779aedb84d8bde4a4bad9bde5fc0044bf07d4e
maxvorontsov581 <maxvorontsov@icloud.com>
2026-03-08 06:06:57 +0300

    vigenere: финальная версия

--- src/lab2/vigenere.py
+++ src/lab2/vigenere.py
@@ -2,28 +2,32 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     ciphertext = []
     keyword = keyword.upper()
     key_len = len(keyword)
-    for i, char in enumerate(plaintext):
-        if char.isalpha():
-            shift = ord(keyword[i % key_len]) - ord('A')
-            if char.isupper():
-                ciphertext.append(chr((ord(char) - ord('A') + shift) % 26 + ord('A')))
+    key_index = 0
+    for ch in plaintext:
+        if ch.isalpha():
+            shift = ord(keyword[key_index % key_len]) - ord('A')
+            if ch.isupper():
+                ciphertext.append(chr((ord(ch) - ord('A') + shift) % 26 + ord('A')))
             else:
-                ciphertext.append(chr((ord(char) - ord('a') + shift) % 26 + ord('a')))
+                ciphertext.append(chr((ord(ch) - ord('a') + shift) % 26 + ord('a')))
+            key_index += 1
         else:
-            ciphertext.append(char)
+            ciphertext.append(ch)
     return ''.join(ciphertext)
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     plaintext = []
     keyword = keyword.upper()
     key_len = len(keyword)
-    for i, char in enumerate(ciphertext):
-        if char.isalpha():
-            shift = ord(keyword[i % key_len]) - ord('A')
-            if char.isupper():
-                plaintext.append(chr((ord(char) - ord('A') - shift) % 26 + ord('A')))
+    key_index = 0
+    for ch in ciphertext:
+        if ch.isalpha():
+            shift = ord(keyword[key_index % key_len]) - ord('A')
+            if ch.isupper():
+                plaintext.append(chr((ord(ch) - ord('A') - shift) % 26 + ord('A')))
             else:
-                plaintext.append(chr((ord(char) - ord('a') - shift) % 26 + ord('a')))
+                plaintext.append(chr((ord(ch) - ord('a') - shift) % 26 + ord('a')))
+            key_index += 1
         else:
-            plaintext.append(char)
+            plaintext.append(ch)
     return ''.join(plaintext)
