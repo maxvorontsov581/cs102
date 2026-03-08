3f141cb51f34e88d7350c09fbb8823b27729dea1
maxvorontsov581 <maxvorontsov@icloud.com>
2026-03-08 05:15:34 +0300

    Реализована функция encrypt_caesar() и decrypt_caesar()

--- src/lab2/caesar.py
+++ src/lab2/caesar.py
@@ -1,32 +1,28 @@
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
-    """
-    Encrypts plaintext using a Caesar cipher.
-    >>> encrypt_caesar("PYTHON")
-    'SBWKRQ'
-    >>> encrypt_caesar("python")
-    'sbwkrq'
-    >>> encrypt_caesar("Python3.6")
-    'Sbwkrq3.6'
-    >>> encrypt_caesar("")
-    ''
-    """
+    """Encrypts plaintext using a Caesar cipher."""
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for char in plaintext:
+        if char.isalpha():
+            if char.isupper():
+                shifted = (ord(char) - ord('A') + shift) % 26 + ord('A')
+            else:
+                shifted = (ord(char) - ord('a') + shift) % 26 + ord('a')
+            ciphertext += chr(shifted)
+        else:
+            ciphertext += char
     return ciphertext
 
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
-    """
-    Decrypts a ciphertext using a Caesar cipher.
-    >>> decrypt_caesar("SBWKRQ")
-    'PYTHON'
-    >>> decrypt_caesar("sbwkrq")
-    'python'
-    >>> decrypt_caesar("Sbwkrq3.6")
-    'Python3.6'
-    >>> decrypt_caesar("")
-    ''
-    """
+    """Decrypts a ciphertext using a Caesar cipher."""
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for char in ciphertext:
+        if char.isalpha():
+            if char.isupper():
+                shifted = (ord(char) - ord('A') - shift) % 26 + ord('A')
+            else:
+                shifted = (ord(char) - ord('a') - shift) % 26 + ord('a')
+            plaintext += chr(shifted)
+        else:
+            plaintext += char
     return plaintext
